f192a6d26fb2bfd7517dfd78de1943da7210abaf
sureshsolanki1989 <30418056+sureshsolanki1989@users.noreply.github.com>
2018-09-11 12:47:26 +0530

    Create Challenges.js

---
+++ Challenges.js
@@ -0,0 +1,24 @@
+function Person(initialAge) {
+  if (initialAge >= 0) {
+    this.age = initialAge;
+  } else {
+    process.stdout.write("Age is not valid, setting age to 0.\n")
+    this.age = 0;
+  }
+
+  this.amIOld=function(){
+    if (this.age < 13) {
+      process.stdout.write("You are young.\n");
+    }
+    else if (this.age < 18) {
+      process.stdout.write("You are a teenager.\n");
+    }
+    else {
+      process.stdout.write("You are old.\n");
+    }
+
+  };
+  this.yearPasses=function(){
+    this.age++;
+  };
+}
